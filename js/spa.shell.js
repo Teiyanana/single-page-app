2e1e86ff5400a516c6fb67c619fb9e586780cc30
Teiyanana <312248024@qq.com>
2019-07-28 16:59:40 +0800

    管理聊天滑块

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -35,12 +35,68 @@ spa.shell = (function() {
         <div class="spa-shell-foot"></div>
         <div class="spa-shell-chat"></div>
         <div class="spa-shell-modal"></div>
-        `
+        `,
+      // 配置滑块运动的速度和高度，在模块配置映射中保存收起和展开的时间和高度
+      chat_extend_time: 250,
+      chat_retract_time: 300,
+      chat_extent_height: 450,
+      chat_retract_height: 15,
+      chat_extended_title: '点击收起',
+      chat_retracted_title: '点击展开'
     },
-    stateMap = { $container: null }, //存储整个模块共享的动态信息
+    stateMap = { //存储整个模块共享的动态信息
+      $container: null,
+      is_chat_retracted: true // true: 聊天滑块收缩状态，fals: 展开状态
+    },
     jqueryMap = {}, // 存储jQuery集合缓存
     setJqueryMap, // 声明所有模块的作用域内变量。
+    toggleChat, // 切换聊天模块变量
+    onClickChat, // 点击聊天模块事件处理程序
     initModule;
+
+  /**
+   * DOM 方法
+   */
+  toggleChat = function (do_extend, callback) {
+    var
+      px_chat_ht = jqueryMap.$chat.height(),
+      is_open = px_chat_ht === configMap.chat_extent_height,
+      is_closed = px_chat_ht === configMap.chat_retract_height,
+      is_Sliding = !is_open && !is_closed;
+    // 消除冲突
+    if (is_Sliding) {
+      return false;
+    }
+    // 开始展开聊天滑动
+    if (do_extend) {
+      jqueryMap.$chat.animate({
+        height: configMap.chat_extent_height
+      },
+        configMap.chat_extend_time,
+        function () {
+          jqueryMap.$chat.attr('title', configMap.chat_extended_title);
+          stateMap.is_chat_retracted = false;
+          if (callback) {
+            callback(jqueryMap.$chat);
+          }
+        });
+      return true;
+    }
+
+    // 开始收起聊天滑块
+    jqueryMap.$chat.animate({
+      height: configMap.chat_retract_height
+    },
+      configMap.chat_retract_time,
+      function () {
+        jqueryMap.$chat.attr('title', configMap.chat_retracted_title);
+        stateMap.is_chat_retracted = true;
+        if (callback) {
+          callback(jqueryMap.$chat);
+        }
+      });
+    return true;
+  }
 
   /**
    * 使用setJqueryMap来缓存jQuery集合。
@@ -50,13 +106,20 @@ spa.shell = (function() {
   setJqueryMap = function() {
     var $container = stateMap.$container;
     jqueryMap = {
-      $container: $container
+      $container: $container,
+      $chat: $container.find('.spa-shell-chat')
     };
   };
+
+  
 
   /**
    * 为jQuery事件处理函数保留的Event handlers区块
    */
+  onClickChat = function(event) {
+    toggleChat(stateMap.is_chat_retracted);
+    return false;
+  }
 
   /**
    * 创建initModule公开方法，用于初始化模块。
@@ -65,6 +128,12 @@ spa.shell = (function() {
     stateMap.$container = $container;
     $container.html(configMap.main_html);
     setJqueryMap();
+
+    // 默认聊天滑块收起，并注册聊天滑块的点击事件
+    stateMap.is_chat_retracted = true;
+    jqueryMap.$chat
+      .attr('title', configMap.chat_retracted_title)
+      .click(onClickChat);
   };
 
   return {
